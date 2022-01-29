3afba1630b64175079a985153f2081ab2c670db1
MCris29 <crismax0629@gmail.com>
2022-01-28 20:24:01 -0500

    Config dark/light mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,59 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import { useEffect, useMemo, useState } from 'react';
+import {
+  createTheme,
+  PaletteMode,
+  ThemeProvider,
+  useTheme,
+  CssBaseline,
+  Typography,
+  Badge,
+  Box,
+  Divider,
+} from '@mui/material';
+
+import MailIcon from '@mui/icons-material/Mail';
+
+import '@/styles/globals.css';
+import Navigation from '@/components/Navigation';
+
+import {
+  getStoredTheme,
+  getThemeOptions,
+  setStoredTheme,
+} from '@/utils/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [mode, setMode] = useState<PaletteMode>('dark'); // default is dark mode
+
+  useEffect(() => {
+    const storedTheme = getStoredTheme();
+
+    if (storedTheme) {
+      setMode(storedTheme);
+    }
+  }, []);
+
+  // Update the theme only if it changes
+  const theme = useMemo(() => createTheme(getThemeOptions(mode)), [mode]);
+
+  const customTheme = useTheme(); // for use in other components - could potentially use theme
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Navigation
+        mode={mode}
+        onChange={() => {
+          const newMode: PaletteMode = mode === 'dark' ? 'light' : 'dark';
+          setMode(newMode);
+          setStoredTheme(newMode);
+        }}
+      />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,3 +1,33 @@
+// import Document, {
+//   DocumentContext,
+//   Html,
+//   Head,
+//   Main,
+//   NextScript,
+// } from "next/document";
+
+// class MyDocument extends Document {
+//   static async getInitialProps(ctx: DocumentContext) {
+//     const initialProps = await Document.getInitialProps(ctx);
+
+//     return initialProps;
+//   }
+
+//   render() {
+//     return (
+//       <Html>
+//         <Head />
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
+
+// export default MyDocument;
+
 import Document, {
   DocumentContext,
   Html,
@@ -16,7 +46,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
