409b1f9f065fd2bc8c2c4d557e3e457174d76def
MCris29 <crismax0629@gmail.com>
2022-05-03 11:57:13 -0500

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,32 @@
+import reacr from "react";
+import styles from "@/styles/Home.module.scss";
+import { useTheme } from "@mui/material";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import EmailIcon from "@mui/icons-material/Email";
+
+const Footer = () => {
+  const socialNetworks = [
+    { icon: <GitHubIcon />, text: "GitHub" },
+    { icon: <LinkedInIcon />, text: "LinkedIn" },
+    { icon: <TwitterIcon />, text: "Twiter" },
+    { icon: <EmailIcon />, text: "Gmail" },
+  ];
+
+  return (
+    <>
+      <div className={styles.footer}>
+        {socialNetworks.map((item, key) => {
+          return (
+            <div key={key} className={styles.icon}>
+              {item.icon}
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Footer;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import About from '@/components/About';
 import Skills from '@/components/Skills';
 import Projects from '@/components/Projects';
 import Contact from '@/components/Contact';
+import Footer from '@/components/Footer';
 
 
 const Home: NextPage = () => {
@@ -18,6 +19,7 @@ const Home: NextPage = () => {
       <Skills/>
       <Projects/>
       <Contact/>
+      <Footer/>
     </div>
   );
 };
